d0e98b2728d860e67d6dce304e6f5a916ae43ffd
Zaeli <disraelifilho@gmail.com>
2023-03-02 19:53:47 -0300

    gamer over e restart

--- app/validAttempt.js
+++ app/validAttempt.js
@@ -4,8 +4,20 @@ function verificaSeOChutePossuiUmValorValido() {
 
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Isso não é um número</div>';
-        return;
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+                document.body.style.backgroundImage = "url(./app/game-over.jpg)";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
 
     if (numeroForMaiorQueOValorPermitido(numero)) {
@@ -16,6 +28,7 @@ function verificaSeOChutePossuiUmValorValido() {
         document.body.innerHTML = `
         <h2>Parabéns! Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+        <button class="btn-jogar" id="jogar-novamente">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `<div>Nosso número secreto é menor <i class="fa-solid fa-arrow-down"></i></div></div>`;
@@ -32,3 +45,8 @@ function numeroForMaiorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor;
 }
 
+document.body.addEventListener('click', e => {
+    if (e.target.id === 'jogar-novamente') {
+        window.location.reload();
+    }
+})
